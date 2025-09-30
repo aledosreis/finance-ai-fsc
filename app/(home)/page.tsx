7917052127280be7ba3b6968ebe930b3d2e7b1ca
Alessandro Reis <alessandrodreis23@gmail.com>
2025-09-30 16:50:32 -0300

    feat: ensure get dashboard function is protected

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -22,7 +22,9 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   }
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect("?month=01");
+    redirect(
+      `?month=${(new Date().getMonth() + 1).toString().padStart(2, "0")}`,
+    );
   }
   const dashboard = await getDashboard(month);
   return (
