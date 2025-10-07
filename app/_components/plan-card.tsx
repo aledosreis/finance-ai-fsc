84e16beb3d2c7b6f4fd512c0f7089205dd571243
Alessandro Reis <alessandrodreis23@gmail.com>
2025-10-07 14:59:09 -0300

    fix: plan card show button only when is premium card

--- app/_components/plan-card.tsx
+++ app/_components/plan-card.tsx
@@ -54,7 +54,7 @@ const PlanCard = ({
           </div>
         ))}
 
-        <AcquirePlanButton />
+        {plan.slug !== "basic" && <AcquirePlanButton />}
       </CardContent>
     </Card>
   );
