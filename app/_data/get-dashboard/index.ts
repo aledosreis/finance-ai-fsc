eb5c1acec06983cd0137c73eaf25c8ff79bdd911
Alessandro Reis <alessandrodreis23@gmail.com>
2025-10-09 15:37:51 -0300

    feat: change get-dashbard from prisma to drizzle

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -1,83 +1,119 @@
-import { db } from "@/app/_lib/prisma";
 import { TransactionType } from "@prisma/client";
 import { TotalExpensePerCategory, TransactionPercentagePerType } from "./types";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq, gte, lt, sum } from "drizzle-orm";
+import { transaction } from "@/app/_lib/db/schema";
+import { db } from "@/app/_lib/db";
 
 export const getDashboard = async (month: string) => {
   const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
-  const where = {
-    userId,
-    date: {
-      gte: new Date(`2025-${month}-01`),
-      lt: new Date(`2025-${month}-31`),
-    },
-  };
-  const depositsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { ...where, type: "DEPOSIT" },
-        _sum: { amount: true },
-      })
-    )?._sum?.amount,
-  );
-  const investmentsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { ...where, type: "INVESTMENT" },
-        _sum: { amount: true },
-      })
-    )?._sum?.amount,
-  );
-  const expensesTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: { ...where, type: "EXPENSE" },
-        _sum: { amount: true },
-      })
-    )?._sum?.amount,
-  );
+  const depositsTotal =
+    Number(
+      (
+        await db
+          .select({ sum: sum(transaction.amount) })
+          .from(transaction)
+          .where(
+            and(
+              eq(transaction.userId, userId),
+              gte(transaction.date, new Date(`2025-${month}-01`)),
+              lt(transaction.date, new Date(`2025-${month}-31`)),
+              eq(transaction.type, "DEPOSIT"),
+            ),
+          )
+      )[0].sum,
+    ) || 0;
+  const investmentsTotal =
+    Number(
+      (
+        await db
+          .select({ sum: sum(transaction.amount) })
+          .from(transaction)
+          .where(
+            and(
+              eq(transaction.userId, userId),
+              gte(transaction.date, new Date(`2025-${month}-01`)),
+              lt(transaction.date, new Date(`2025-${month}-31`)),
+              eq(transaction.type, "INVESTMENT"),
+            ),
+          )
+      )[0].sum,
+    ) || 0;
+  const expensesTotal =
+    Number(
+      (
+        await db
+          .select({ sum: sum(transaction.amount) })
+          .from(transaction)
+          .where(
+            and(
+              eq(transaction.userId, userId),
+              gte(transaction.date, new Date(`2025-${month}-01`)),
+              lt(transaction.date, new Date(`2025-${month}-31`)),
+              eq(transaction.type, "EXPENSE"),
+            ),
+          )
+      )[0].sum,
+    ) || 0;
   const balance = depositsTotal - investmentsTotal - expensesTotal;
-  const transactionsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where,
-        _sum: { amount: true },
-      })
-    )?._sum?.amount,
-  );
+  const transactionsTotal =
+    Number(
+      (
+        await db
+          .select({ sum: sum(transaction.amount) })
+          .from(transaction)
+          .where(
+            and(
+              eq(transaction.userId, userId),
+              gte(transaction.date, new Date(`2025-${month}-01`)),
+              lt(transaction.date, new Date(`2025-${month}-31`)),
+            ),
+          )
+      )[0].sum,
+    ) || 0;
   const typesPercentage: TransactionPercentagePerType = {
-    [TransactionType.DEPOSIT]: Math.round(
-      (Number(depositsTotal || 0) / Number(transactionsTotal)) * 100,
-    ),
-    [TransactionType.EXPENSE]: Math.round(
-      (Number(expensesTotal || 0) / Number(transactionsTotal)) * 100,
-    ),
-    [TransactionType.INVESTMENT]: Math.round(
-      (Number(investmentsTotal || 0) / Number(transactionsTotal)) * 100,
-    ),
+    [TransactionType.DEPOSIT]:
+      Math.round((depositsTotal / transactionsTotal) * 100) || 0,
+    [TransactionType.EXPENSE]:
+      Math.round((expensesTotal / transactionsTotal) * 100) || 0,
+    [TransactionType.INVESTMENT]:
+      Math.round((investmentsTotal / transactionsTotal) * 100) || 0,
   };
 
   const totalExpensePerCategory: TotalExpensePerCategory[] = (
-    await db.transaction.groupBy({
-      by: ["category"],
-      where: { ...where, type: "EXPENSE" },
-      _sum: { amount: true },
-    })
+    await db
+      .select({
+        category: transaction.category,
+        sum: sum(transaction.amount),
+      })
+      .from(transaction)
+      .where(
+        and(
+          eq(transaction.userId, userId),
+          gte(transaction.date, new Date(`2025-${month}-01`)),
+          lt(transaction.date, new Date(`2025-${month}-31`)),
+          eq(transaction.type, "EXPENSE"),
+        ),
+      )
+      .groupBy(transaction.category)
   ).map((category) => ({
     category: category.category,
-    totalAmount: Number(category._sum.amount),
-    percentOfTotal: Math.round(
-      (Number(category._sum.amount) / Number(expensesTotal)) * 100,
-    ),
+    totalAmount: Number(category.sum) || 0,
+    percentOfTotal: Math.round((Number(category.sum) / expensesTotal) * 100),
   }));
 
-  const lastTransactions = await db.transaction.findMany({
-    where,
-    orderBy: { date: "desc" },
-    take: 15,
+  const lastTransactions = await db.query.transaction.findMany({
+    where: (transaction, { eq, and, gte, lt }) =>
+      and(
+        eq(transaction.userId, userId),
+        gte(transaction.date, new Date(`2025-${month}-01`)),
+        lt(transaction.date, new Date(`2025-${month}-31`)),
+      ),
+    orderBy: (transaction, { desc }) => [desc(transaction.date)],
+    limit: 15,
   });
 
   return {
